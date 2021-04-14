ff5b694452ca635da1c81beffcfa6ab34789d816
Maksim Kostromin <daggerok@gmail.com>
2021-04-14 22:50:37 +0300

    #GrammarNazi (C) whatthecommit.com

---
+++ docs/src/.vuepress/config.js
@@ -0,0 +1,49 @@
+const { name: title, description, repository } = require('../../package.json')
+const base = process.env.BASE || '/';
+
+module.exports = {
+
+  base,
+
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#title
+   */
+  title,
+
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#description
+   */
+  description: description,
+
+  /**
+   * Extra tags to be injected to the page HTML `<head>`
+   *
+   * ref：https://v1.vuepress.vuejs.org/config/#head
+   */
+  head: [
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+  ],
+
+  /**
+   * Theme configuration, here is the default theme configuration for VuePress.
+   *
+   * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
+   */
+  themeConfig: {
+    repo: repository.url,
+    editLinks: false,
+    docsDir: repository.directory,
+    editLinkText: '',
+    lastUpdated: false,
+  },
+
+  /**
+   * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
+   */
+  plugins: [
+    '@vuepress/plugin-back-to-top',
+    '@vuepress/plugin-medium-zoom',
+  ]
+}
---
+++ docs/src/index.md
@@ -0,0 +1,21 @@
+---
+
+home: true
+heroImage: https://v1.vuepress.vuejs.org/hero.png
+
+tagline: TODO
+
+#actionText: Getting Started →
+#actionLink: /deploy/
+
+#features:
+#- title: Search
+#  details: Want to clean up your laptop drive? Search if there are some build output folders you can wipe  
+#- title: Wipe
+#  details: Disable "dry run" option to clean up all found folders afterall
+
+footer: Made by Maksim Kostromin with ❤️ 🤘
+
+---
+
+TBD...
