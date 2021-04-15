448321b64a70a8f79e743e50e856f28f2cbbe135
Maksim Kostromin <maksim.kostromin@leovegas.com>
2021-04-15 17:03:05 +0300

    SEXY RUSSIAN CODES WAITING FOR YOU TO CALL (C) whatthecommit.com

--- server/src/main/java/com/example/server/ServerApplication.java
+++ server/src/main/java/com/example/server/ServerApplication.java
@@ -10,10 +10,13 @@ import io.rsocket.rpc.testing.protobuf.SimpleService;
 import io.rsocket.rpc.testing.protobuf.SimpleServiceServer;
 import io.rsocket.transport.netty.server.CloseableChannel;
 import io.rsocket.transport.netty.server.TcpServerTransport;
+import lombok.extern.log4j.Log4j2;
 import org.reactivestreams.Publisher;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -23,53 +26,55 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Function;
 
+@Log4j2
 class DefaultSimpleService implements SimpleService {
+
   @Override
   public Mono<Empty> fireAndForget(SimpleRequest message, ByteBuf metadata) {
-    System.out.println("got fireAndForget message -> " + message.getRequestMessage());
+    log.info("got fireAndForget message -> {}", message.getRequestMessage());
     return Mono.just(Empty.getDefaultInstance());
   }
 
   @Override
   public Mono<SimpleResponse> requestReply(SimpleRequest message, ByteBuf metadata) {
-    return Mono.fromCallable(
-        () ->
-            SimpleResponse.newBuilder()
-                          .setResponseMessage("we got requestReply message -> " + message.getRequestMessage())
-                          .build());
+    log.info("requestReply: -> {}", message);
+    return Mono.fromCallable(() -> SimpleResponse
+        .newBuilder()
+        .setResponseMessage("we got requestReply message -> " + message.getRequestMessage())
+        .build());
   }
 
   @Override
   public Mono<SimpleResponse> streamingRequestSingleResponse(Publisher<SimpleRequest> messages, ByteBuf metadata) {
+    log.info("streamingRequestSingleResponse ->");
     return Flux.from(messages)
                .windowTimeout(10, Duration.ofSeconds(500))
                .take(1)
                .flatMap(Function.identity())
-               .reduce(
-                   new ConcurrentHashMap<Character, AtomicInteger>(),
-                   (map, s) -> {
-                     char[] chars = s.getRequestMessage().toCharArray();
-                     for (char c : chars) {
-                       map.computeIfAbsent(c, _c -> new AtomicInteger()).incrementAndGet();
-                     }
-                     return map;
-                   })
-               .map(
-                   map -> {
-                     StringBuilder builder = new StringBuilder();
-                     map.forEach(
-                         (character, atomicInteger) -> builder.append(
-                             String.format("character -> %s, count -> %d%n",
-                                           character, atomicInteger.get())));
-
-                     String s = builder.toString();
-                     return SimpleResponse.newBuilder().setResponseMessage(s).build();
-                   });
+               .reduce(new ConcurrentHashMap<Character, AtomicInteger>(), (map, s) -> {
+                 char[] chars = s.getRequestMessage().toCharArray();
+                 for (char c : chars) {
+                   map.computeIfAbsent(c, _c -> new AtomicInteger()).incrementAndGet();
+                 }
+                 return map;
+               })
+               .map(map -> {
+                 StringBuilder builder = new StringBuilder();
+                 map.forEach((character, atomicInteger) -> builder.append(
+                     String.format("character -> %s, count -> %d%n",
+                                   character, atomicInteger.get()))
+                 );
+                 map.forEach((character, atomicInteger) -> log
+                     .info("character -> {}, count -> {}", character, atomicInteger.get()));
+                 String s = builder.toString();
+                 return SimpleResponse.newBuilder().setResponseMessage(s).build();
+               });
   }
 
   @Override
   public Flux<SimpleResponse> requestStream(SimpleRequest message, ByteBuf metadata) {
     String requestMessage = message.getRequestMessage();
+    log.info("requestStream -> {}", requestMessage);
     return Flux.interval(Duration.ofMillis(200))
                .onBackpressureDrop()
                .map(i -> i + " - got message - " + requestMessage)
@@ -82,23 +87,36 @@ class DefaultSimpleService implements SimpleService {
   }
 }
 
+@Configuration
+class RSocketConfig {
+
+  @Bean
+  SimpleServiceServer serviceServer() {
+    return new SimpleServiceServer(new DefaultSimpleService(), Optional.empty(), Optional.empty());
+  }
+
+  @Bean
+  RSocketFactory.Start<CloseableChannel> starter(SimpleServiceServer serviceServer) {
+    return RSocketFactory.receive()
+                         .acceptor((setup, sendingSocket) -> Mono.just(new RequestHandlingRSocket(serviceServer)))
+                         .transport(TcpServerTransport.create(7070));
+  }
+
+  // @Bean
+  Mono<CloseableChannel> rSocket(RSocketFactory.Start<CloseableChannel> starter) {
+    return starter.start();
+  }
+}
+
 @SpringBootApplication
 public class ServerApplication {
 
   public static void main(String[] args) {
-    SimpleServiceServer serviceServer = new SimpleServiceServer(new DefaultSimpleService(), Optional.empty(), Optional.empty());
-    CloseableChannel closeableChannel =
-        RSocketFactory.receive()
-                      .acceptor(
-                          (setup, sendingSocket) -> Mono.just(new RequestHandlingRSocket(serviceServer)))
-                      .transport(TcpServerTransport.create(7070))
-                      .start()
-                      .block();
-
-    // Block so we don't exit
-    closeableChannel.onClose().block();
-
     ConfigurableApplicationContext context = SpringApplication.run(ServerApplication.class, args);
-    context.close();
+    RSocketFactory.Start<CloseableChannel> starter = context.getBean(RSocketFactory.Start.class);
+    starter.start()
+           .block()
+           .onClose()
+           .block();
   }
 }
